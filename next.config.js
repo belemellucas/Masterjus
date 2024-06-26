66e0a271a37b52033bfc605a929e0f6e588cc18c
belelemllukas <lukasbelemel@gmail.com>
2024-06-26 19:25:22 -0300

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['avatars.githubusercontent.com', 'images.pexels.com', 'www.wordstream.com']
+    }
+}
+
+module.exports = nextConfig
