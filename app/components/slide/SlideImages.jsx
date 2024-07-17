55d5bb5ce343ad9411ea9e775f04e17fe1d143ba
belelemllukas <lukasbelemel@gmail.com>
2024-07-17 14:52:15 -0300

    atualizacao

--- app/components/slide/SlideImages.jsx
+++ app/components/slide/SlideImages.jsx
@@ -1,30 +1,45 @@
 "use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const SlideImages = ({ infoSite }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    if (!infoSite || infoSite.length === 0) return;
+
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % infoSite[0].imageAnex.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [infoSite]); 
+
+  useEffect(() => {
+  }, [currentSlide]); 
+
+  if (!infoSite || infoSite.length === 0) {
+    return null; 
+  }
+
   return (
-    <>
-      <div>
-        {infoSite?.length > 0 &&
-          infoSite.map((info) => (
-            <div
-              key={info?.id}
-              className="relative w-full h-[452px] overflow-hidden"
-            >
-              {info.imageAnex.map((base64String, index) => (
-                <Image
-                  key={index}
-                  src={`data:image/jpeg;base64,${base64String}`}
-                  alt={`Image ${index}`}
-                  width={500}
-                  height={500}
-                  className="object-cover"
-                />
-              ))}
-            </div>
-          ))}
+    <div className="relative w-full h-[452px] overflow-hidden">
+      <div className="absolute inset-0 transition-opacity duration-1000">
+        {infoSite[0].imageAnex.map((base64String, imageIndex) => (
+          <div
+            key={imageIndex}
+            className={`absolute inset-0 ${imageIndex === currentSlide ? 'opacity-100' : 'opacity-0'}`}
+          >
+            <Image
+              src={`data:image/jpeg;base64,${base64String}`}
+              alt={`Image ${imageIndex}`}
+              layout="fill"
+              className="object-cover w-full h-full"
+            />
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
